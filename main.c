c262b62112f121dd88b6b38c9d30ad077fa9cbfa
unknown <Administrador@SAN-GR2S6S2.usersad.everis.int>
2019-04-23 20:28:59 -0400

    commit echo por juan fuentes

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+
+    int r,j, f[5]={1,2,3,4,60};
+
+    for ( r = 0; r < 5; ++r) {
+        printf("%i. digite un numero: ", 1+r);
+        scanf("%i", &f[r] );
+    }
+
+
+    for ( j = 0; j < 5 ; ++j) {
+        printf("%i", f[j]);
+    }
+    getch();
+
+    return 0;
+}
